100ad5f6951345e184ea51e0ac2df91bbe5081a5
nickstu14 <nickstu14@hotmail.co.uk>
2020-01-13 21:16:56 +0000

    Added something
    Too long ago to remember

--- Encription/Encription/Main.cpp
+++ Encription/Encription/Main.cpp
@@ -11,7 +11,7 @@ int main()
 	std::vector<EncodeData*> m_EncodedData;
 	std::vector<int> m_BinaryLine;
 	int m_NewBit;
-	int m_Counter=0;
+	int m_Counter = 0;
 
 	//Get user input
 	std::cout << "Enter Password: ";
@@ -36,36 +36,50 @@ int main()
 		}
 	}
 
-	for (int m_I =0; m_I < m_BinaryLine.size(); m_I++)
+	for (int m_I = 0; m_I < m_BinaryLine.size(); m_I++)
 	{ //debug printout
 		std::cout << m_BinaryLine.at(m_I);
 	}
+
+	std::cout << std::endl << "Bits:" << m_BinaryLine.size() << std::endl;
 
 	//Ask user for new bit to conver to
 	std::cout << "\nConvert to bit: ";
 	std::cin >> m_NewBit;
 
 	char m_Val = 'A';
-	
-	
-	do
-	{
-		EncodeData* m_TempEncode = new EncodeData();
-		std::vector<int> m_NewBitBinary;
-		for (int m_I = 0; m_I <= m_NewBit-1; m_I++)
-		{
-			m_NewBitBinary.push_back(m_BinaryLine.at(m_Counter));
-			m_Counter++;
-		}
-		if (m_NewBitBinary.size() < m_NewBit)
-		{
-			for (int m_J = m_BinaryLine.size(); m_J < m_NewBit-1; m_J++)
-				m_NewBitBinary.push_back(0);
-		}
-		m_TempEncode->SetupBinary(m_NewBitBinary, m_NewBit);
-		m_EncodedData.push_back(m_TempEncode);
 
-	} while (m_Counter <= m_BinaryLine.size());
+	for (int m_I = 0; m_I <= m_BinaryLine.size() - 1; m_I++)
+	{
+		//if ((m_I + m_NewBit) <= m_BinaryLine.size())
+		{
+			int m_J;
+			EncodeData* m_TempEncode = new EncodeData();
+			std::vector<int> m_NewBitBinary;
+			for (m_J = 0; m_J <= m_NewBit - 1; m_J++)
+			{
+				if ((m_I + 1) <= m_BinaryLine.size())
+				{
+					m_NewBitBinary.push_back(m_BinaryLine.at(m_I + m_J));
+					std::cout << m_I + m_J << " " << m_BinaryLine.at(m_I + m_J) << std::endl;
+				}
+			}
+			if (m_NewBitBinary.size() < m_NewBit)
+			{
+				for (int m_K = m_NewBitBinary.size(); m_K <= m_NewBit - 1; m_K++)
+					m_NewBitBinary.push_back(0);
+			}
+
+			std::cout << std::endl << "---" << std::endl;
+
+			m_TempEncode->SetupBinary(m_NewBitBinary, m_NewBit);
+			m_EncodedData.push_back(m_TempEncode);
+			m_I += m_J - 1;
+		}
+	}
+
+	
+
 
 	for (EncodeData* m_ED : m_EncodedData)
 	{
